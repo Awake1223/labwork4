c66072070423eb077eaaf6b4d6cb6f83f02097c4
George <zhora.sviridov.05@bk.ru>
2024-10-19 15:01:05 +0500

    the final commit

--- studies/views.py
+++ studies/views.py
@@ -2,6 +2,7 @@ import json
 import os
 import re
 from django.shortcuts import render, redirect
+from django.template.context_processors import request
 
 JSON_FILE = 'uploads/data.json'  # Укажите путь к вашему JSON файлу
 
@@ -32,40 +33,52 @@ def index(request):
     data_list = read_data_from_json()  # Чтение данных из JSON файла
 
     if request.method == 'POST':
-        title = request.POST.get('title')
-        author = request.POST.get('author')
-        year = request.POST.get('year')
-
-        # Проверка наличия всех данных и валидация
-        if title and author and year:
+        if 'file' in request.FILES:
+            uploaded_file = request.FILES['file']
             try:
-                year = int(year)
-                if year < 1924 or year > 2100:
-                    raise ValueError("Год должен быть в пределах от 1924 до 2100.")
-                if not valid_name_pattern.match(title):
-                    error_message = "Поле 'Название' может содержать только английские и русские буквы."
-                    return render(request, 'index.html', {'data_list': data_list, 'error': error_message})
-                if not valid_name_pattern.match(author):
-                    error_message = "Поле 'Автор' может содержать только английские и русские буквы."
-                    return render(request, 'index.html', {'data_list': data_list, 'error': error_message})
-                # Сохраняем новые данные в JSON файл
-                new_entry = {
-                    'title': title,
-                    'author': author,
-                    'year': year
-                }
-
-                # Апдейт списка и запись в файл
-                data_list.append(new_entry)
-                write_data_to_json(data_list)
-
-                # Перенаправление после успешной отправки формы
-                return redirect('index')
-
-            except ValueError as e:
-                return render(request, 'index.html', {'error': str(e), 'data_list': data_list})
-
+                uploaded_data = json.load(uploaded_file)
+                if isinstance(uploaded_data, list):
+                    data_list.extend(uploaded_data)
+                    write_data_to_json(data_list)
+                else:
+                    return render(request, 'index.html', {'error': "Загруженный файл должен содержать список объектов.", 'data_list': data_list})
+            except json.JSONDecodeError:
+                return render(request, 'index.html', {'error': "Не удалось прочитать файл JSON.", 'data_list': data_list})
         else:
-            return render(request, 'index.html', {'error': "Заполните все поля.", 'data_list': data_list})
+            title = request.POST.get('title')
+            author = request.POST.get('author')
+            year = request.POST.get('year')
+
+            # Проверка наличия всех данных и валидация
+            if title and author and year:
+                try:
+                    year = int(year)
+                    if year < 1924 or year > 2100:
+                        raise ValueError("Год должен быть в пределах от 1924 до 2100.")
+                    if not valid_name_pattern.match(title):
+                        error_message = "Поле 'Название' может содержать только английские и русские буквы."
+                        return render(request, 'index.html', {'data_list': data_list, 'error': error_message})
+                    if not valid_name_pattern.match(author):
+                        error_message = "Поле 'Автор' может содержать только английские и русские буквы."
+                        return render(request, 'index.html', {'data_list': data_list, 'error': error_message})
+                    # Сохраняем новые данные в JSON файл
+                    new_entry = {
+                        'title': title,
+                        'author': author,
+                        'year': year
+                    }
+
+                    # Апдейт списка и запись в файл
+                    data_list.append(new_entry)
+                    write_data_to_json(data_list)
+
+                    # Перенаправление после успешной отправки формы
+                    return redirect('index')
+
+                except ValueError as e:
+                    return render(request, 'index.html', {'error': str(e), 'data_list': data_list})
+
+            else:
+                return render(request, 'index.html', {'error': "Заполните все поля.", 'data_list': data_list})
 
     return render(request, 'index.html', {'data_list': data_list})
